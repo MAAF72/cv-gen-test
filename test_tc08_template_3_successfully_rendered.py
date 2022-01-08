79d014ad67019595ff4cf620015c58fdc7c00107
Ilham Izzul Hadyan <ilhamizzul@gmail.com>
2022-01-08 19:07:32 +0700

    change deprecated function into available one in TC08

--- test_tc08_template_3_successfully_rendered.py
+++ test_tc08_template_3_successfully_rendered.py
@@ -6,47 +6,39 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.options import Options
 
-## Setup chrome options
-chrome_options = Options()
-chrome_options.add_argument('--no-sandbox')
+def test_template_3_successfuly_rendered():
+    chrome_options = Options()
+    chrome_options.add_argument('--no-sandbox')
 
-# Set path to chromedriver as per your configuration
-webdriver_service = Service("C:\\Program Files (x86)\\Google\\Chrome\\Application\\chromedriver.exe")
+    # Set path to chromedriver as per your configuration
+    webdriver_service = Service("C:\\Program Files (x86)\\Google\\Chrome\\Application\\chromedriver.exe")
+    browser = webdriver.Chrome(service=webdriver_service, options=chrome_options)
 
-# Choose Chrome Browser
-browser = webdriver.Chrome(service=webdriver_service, options=chrome_options)
+    # Wait for initialize, in seconds
+    wait = WebDriverWait(browser, 10)
 
-# Wait for initialize, in seconds
-wait = WebDriverWait(browser, 10)
+    # Get page
+    browser.get("http://cv-gen-dipl.herokuapp.com:80")
 
-# Get page
-browser.get("http://cv-gen-dipl.herokuapp.com:80")
+    # Extract title from page and print
+    title = browser.title
 
-# Extract title from page and print
-title = browser.title
+    # insert unique code
+    browser.find_element(By.ID, "input-unique-code").send_keys("W6FK-ZRM5-H20C")
+    browser.find_element(By.ID, "edit-cv").click()
+    time.sleep(5)
 
-# insert unique code
-browser.find_element_by_id("input-unique-code").send_keys("W6FK-ZRM5-H20C")
-browser.find_element_by_id("edit-cv").click()
-time.sleep(5)
+    # to next page
+    browser.find_element(By.ID, "btn-choose-template").click()
 
-# to next page
-browser.find_element_by_id("btn-choose-template").click()
+    # select template
+    wait.until(EC.presence_of_element_located((By.ID, "template-formal")))
+    browser.find_element(By.ID, "template-formal").click()
+    time.sleep(1)
+    browser.find_element(By.ID, "get-cv").click()
 
-# select template
-wait.until(EC.presence_of_element_located((By.ID, "template-formal")))
-browser.find_element_by_id("template-formal").click()
-time.sleep(1)
-browser.find_element_by_id("get-cv").click()
+    wait.until(EC.alert_is_present())
+    browser.switch_to.alert.accept()
 
-wait.until(EC.alert_is_present())
-browser.switch_to.alert.accept()
-
-if browser.find_element_by_xpath('//*[@id="cv-preview-section"]/embed') :
-    print('test success')
-else:
-    print('test failed')
-
-#Wait for 10 seconds
-time.sleep(10)
-browser.quit()
+    assert browser.find_element(By.XPATH, '//*[@id="cv-preview-section"]/embed'), 'CV template is not rendered'
+    browser.quit()
