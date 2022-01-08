4bd5b32ab78b903db3a0c59d3ec248209ae8b0ee
Ilham Izzul Hadyan <ilhamizzul@gmail.com>
2022-01-08 19:51:17 +0700

    change TC10 file content into a function

--- test_tc10_download_cv_success.py
+++ test_tc10_download_cv_success.py
@@ -6,45 +6,38 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.options import Options
 
-## Setup chrome options
-chrome_options = Options()
-chrome_options.add_argument('--no-sandbox')
+def test_download_cv():
+    chrome_options = Options()
+    chrome_options.add_argument('--no-sandbox')
 
-# Set path to chromedriver as per your configuration
-webdriver_service = Service("C:\\Program Files (x86)\\Google\\Chrome\\Application\\chromedriver.exe")
+    # Set path to chromedriver as per your configuration
+    webdriver_service = Service("C:\\Program Files (x86)\\Google\\Chrome\\Application\\chromedriver.exe")
+    browser = webdriver.Chrome(service=webdriver_service, options=chrome_options)
 
-# Choose Chrome Browser
-browser = webdriver.Chrome(service=webdriver_service, options=chrome_options)
+    # Wait for initialize, in seconds
+    wait = WebDriverWait(browser, 12)
 
-# Wait for initialize, in seconds
-wait = WebDriverWait(browser, 10)
+    # Get page
+    browser.get("http://cv-gen-dipl.herokuapp.com:80")
 
-# Get page
-browser.get("http://cv-gen-dipl.herokuapp.com:80")
+    # insert unique code
+    browser.find_element(By.ID, "input-unique-code").send_keys("W6FK-ZRM5-H20C")
+    browser.find_element(By.ID, "edit-cv").click()
+    time.sleep(5)
 
-# Extract title from page and print
-title = browser.title
+    # to next page
+    browser.find_element(By.ID, "btn-choose-template").click()
 
-# insert unique code
-browser.find_element_by_id("input-unique-code").send_keys("W6FK-ZRM5-H20C")
-browser.find_element_by_id("edit-cv").click()
-time.sleep(5)
+    # select template
+    wait.until(EC.presence_of_element_located((By.ID, "template-formal")))
+    browser.find_element(By.ID, "template-formal").click()
+    time.sleep(1)
+    browser.find_element(By.ID, "get-cv").click()
 
-# to next page
-browser.find_element_by_id("btn-choose-template").click()
+    wait.until(EC.alert_is_present())
+    browser.switch_to.alert.accept()
 
-# select template
-wait.until(EC.presence_of_element_located((By.ID, "template-formal")))
-browser.find_element_by_id("template-formal").click()
-time.sleep(1)
-browser.find_element_by_id("get-cv").click()
-
-wait.until(EC.alert_is_present())
-browser.switch_to.alert.accept()
-
-wait.until(EC.presence_of_element_located((By.ID, "btn-download-cv")))
-browser.find_element_by_id("btn-download-cv").click()
-
-#Wait for 10 seconds
-time.sleep(10)
-browser.quit()
+    wait.until(EC.presence_of_element_located((By.ID, "btn-download-cv")))
+    browser.find_element(By.ID, "btn-download-cv").click()
+    time.sleep(10)
+    browser.quit()
