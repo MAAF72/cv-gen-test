461232a09745956f5f1b85a0458cc6b68af96212
Muhammad Abdurrohman Al Fatih <abdurrahmanalfatih72@gmail.com>
2022-01-09 14:03:22 +0700

    refine: update tc01

--- test_tc01_input_data.py
+++ test_tc01_input_data.py
@@ -68,51 +68,19 @@ def test_tc01_input_data():
     fill('#form-customer #job', 'Quality Assurance')
     fill('#form-customer #deskripsi', 'Tester')
 
-    fill('#form-socialmedia .content-socialmedia:last-child #nama', 'Facebook')
-    fill('#form-socialmedia .content-socialmedia:last-child #link', 'fb.com/john.doe')
-    click('#add-socialmedia')
-    fill('#form-socialmedia .content-socialmedia:last-child #nama', 'Instagram')
-    fill('#form-socialmedia .content-socialmedia:last-child #link', 'ig.com/john.doe')
+    click('#form-socialmedia .content-socialmedia:last-child #delete-socialmedia')
 
-    fill('#form-edukasi .content-edukasi:last-child #jenjang', 'S1')
-    fill('#form-edukasi .content-edukasi:last-child #instansi', 'Telkom University')
-    fill('#form-edukasi .content-edukasi:last-child #tahun_mulai', '09/03/2018')
-    fill('#form-edukasi .content-edukasi:last-child #tahun_selesai', '01/01/2022')
-    fill('#form-edukasi .content-edukasi:last-child #deskripsi', 'S1 Informatika with 3.99 GPA')
+    click('#form-edukasi .content-edukasi:last-child #delete-edukasi')
 
-    fill('#form-penghargaan .content-penghargaan:last-child #nama', 'Best Apps')
-    fill('#form-penghargaan .content-penghargaan:last-child #instansi', 'Arkavidia 5.0')
-    fill('#form-penghargaan .content-penghargaan:last-child #tahun', '01/01/2019')
-    fill('#form-penghargaan .content-penghargaan:last-child #deskripsi', 'CV Generator get Best Apps award at annualy informatics competition Arkavidia 5.0, held by Institut Teknologi Bandung')
+    click('#form-penghargaan .content-penghargaan:last-child #delete-penghargaan')
 
-    fill('#form-pengalaman .content-pengalaman:last-child #nama', 'Junior Software Engineer')
-    fill('#form-pengalaman .content-pengalaman:last-child #instansi', 'PT Proclub Studio Indonesia')
-    fill('#form-pengalaman .content-pengalaman:last-child #tahun_mulai', '01/04/2021')
-    fill('#form-pengalaman .content-pengalaman:last-child #tahun_selesai', '01/09/2021')
-    fill('#form-pengalaman .content-pengalaman:last-child #deskripsi', 'Do backend stuff with golang and some dev-op things')
+    click('#form-pengalaman .content-pengalaman:last-child #delete-pengalaman')
 
-    fill('#form-rujukan .content-rujukan:last-child #nama', 'Doe Jhon')
-    fill('#form-rujukan .content-rujukan:last-child #instansi', 'PT Haha Hihi Project')
-    fill('#form-rujukan .content-rujukan:last-child #no_hp', '081291734621')
-    fill('#form-rujukan .content-rujukan:last-child #email', 'doe.jhon@hahahihi.com')
+    click('#form-rujukan .content-rujukan:last-child #delete-rujukan')
 
-    fill('#form-bahasa .content-bahasa:last-child #nama', 'Indonesia')
-    select('#form-bahasa .content-bahasa:last-child #level', 'Native')
-    click('#add-bahasa')
-    fill('#form-bahasa .content-bahasa:last-child #nama', 'English')
-    select('#form-bahasa .content-bahasa:last-child #level', 'Professional')
-    click('#add-bahasa')
-    fill('#form-bahasa .content-bahasa:last-child #nama', 'Java')
-    select('#form-bahasa .content-bahasa:last-child #level', 'Elementary')
+    click('#form-bahasa .content-bahasa:last-child #delete-bahasa')
 
-    fill('#form-kemampuan .content-kemampuan:last-child #nama', 'Programming')
-    click('#add-kemampuan')
-    fill('#form-kemampuan .content-kemampuan:last-child #nama', 'Problem Solving')
-    click('#add-kemampuan')
-    fill('#form-kemampuan .content-kemampuan:last-child #nama', 'Web Designing')
-    click('#add-kemampuan')
-    fill('#form-kemampuan .content-kemampuan:last-child #nama', 'Researching')
-
+    click('#form-kemampuan .content-kemampuan:last-child #delete-kemampuan')
 
     browser.execute_script("window.scroll(0, 0);")
 
